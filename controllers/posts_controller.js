332154435db04a5f7a978f3ebec43dd15da048af
Austin <joha0033@gmail.com>
2018-07-03 13:23:35 -0600

    cleaning

---
+++ controllers/posts_controller.js
@@ -0,0 +1,59 @@
+const Post = require('../models/post')
+const {posts} = require('./post_seeds')
+
+
+module.exports ={
+  seed: async (req, res, next) => {
+    
+    const seedPosts = (seeds) => {
+      seeds.map((post)=>{
+        let newPost = new Post(post)
+        newPost.save();
+      })
+    }
+
+    return process.env.NODE_ENV !== 'production'  
+    ? ( Post.remove({}).exec(), 
+          seedPosts(posts), 
+          res.json({msg: 'Database cleared and seeded!'})) 
+    : ( res.json({msg: 'Your environment is in Production, cannot kill & reseed'}) )
+   
+  },
+  create: async (req, res, next) => {
+    console.log('req.body',req.body);
+    
+    const { title, content, category, level } = req.body
+
+    console.log('title, content',title, content);
+
+    let titleTaken = await Post.findOne({ title  })
+    console.log('titleTaken', titleTaken);
+
+    if(titleTaken){
+      return res.status(403).json({error: 'title is taken...'})
+    }
+
+    const newPost = new Post({
+      title,
+      content,
+      category,
+      level
+    })
+
+    console.log('newPost', newPost);
+
+    await newPost.save()
+
+    res.status(200).json({
+      got:  req.body
+    })
+  },
+
+  getAll: async (req, res, next) => {
+
+    let posts = await Post.find({})
+    console.log(posts)
+    res.send(posts)
+
+  }
+}
