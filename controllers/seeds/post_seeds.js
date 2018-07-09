6b3c7f71ac65fdab101c73a4902a518a084e064c
Austin <joha0033@gmail.com>
2018-07-09 13:19:33 -0600

    moved seed data

---
+++ controllers/seeds/post_seeds.js
@@ -0,0 +1,148 @@
+const posts = [
+    {
+        "title": "Github Basics",
+        "user_id": "5b43b25044e210cc34e8ab0a",
+        "content": "coming soon",
+        "category": "Github",
+        "level": "Beginner",
+        "comments": ['5b43b3dc55e650ccfd1eb722', '5b43b3dc55e650ccfd1eb723', '5b43b3dc55e650ccfd1eb724'],
+        "upvotes": 5,
+        "downvotes": 1,
+        "__v": 0
+    },
+    {
+        "title": "Github Introduction",
+        "user_id": "5b43b25044e210cc34e8ab0a",
+        "content": "coming soon",
+        "category": "Github",
+        "level": "Beginner",
+        "__v": 0
+    },
+    {
+        "title": "Github Add/Commit/Push",
+        "user_id": "5b43b25044e210cc34e8ab0a",
+        "content": "coming soon",
+        "category": "Github",
+        "level": "Beginner",
+        "__v": 0
+    },
+    {
+        "title": "Github Roll-back/Merge Branches",
+        "user_id": "5b43b25044e210cc34e8ab0a",
+        "content": "coming soon",
+        "category": "Github",
+        "level": "Intermediate",
+        "__v": 0
+    },
+    {
+        "title": "CLI Intro",
+        "user_id": "5b43b25044e210cc34e8ab0b",
+        "content": "coming soon",
+        "category": "Command Line",
+        "level": "Beginner",
+        "__v": 0
+    },
+    {
+        "title": "CLI Basics",
+        "user_id": "5b43b25044e210cc34e8ab0b",
+        "content": "coming soon",
+        "category": "Command Line",
+        "level": "Beginner",
+        "comments": ['5b43b3dc55e650ccfd1eb722', '5b43b3dc55e650ccfd1eb723', '5b43b3dc55e650ccfd1eb724'],
+        "upvotes": 2,
+        "downvotes": 1,
+        "__v": 0
+    },
+    {
+        "title": "CLI Creating Directories",
+        "user_id": "5b43b25044e210cc34e8ab0b",
+        "content": "coming soon",
+        "category": "Command Line",
+        "level": "Beginner",
+        "__v": 0
+    },
+    {
+        "title": "CLI Creating and writing to files",
+        "user_id": "5b43b25044e210cc34e8ab0b",
+        "content": "coming soon",
+        "category": "Command Line",
+        "level": "Intermediate",
+        "__v": 0
+    },
+    {
+        "title": "CLI and NPM",
+        "user_id": "5b43b25044e210cc34e8ab0b",
+        "content": "coming soon",
+        "category": "Command Line",
+        "level": "Intermediate",
+        "__v": 0
+    },
+    {
+        "title": "CLI Opening Files/Applications",
+        "user_id": "5b43b25044e210cc34e8ab0b",
+        "content": "coming soon",
+        "category": "Command Line",
+        "level": "Intermediate",
+        "__v": 0
+    },
+    {
+        "title": "CLI and VIM",
+        "user_id": "5b43b25044e210cc34e8ab0b",
+        "content": "coming soon",
+        "category": "Command Line",
+        "level": "Advanced",
+        "__v": 0
+    },
+    {
+        "title": "Text Editor options",
+        "user_id": "5b43b25044e210cc34e8ab0e",
+        "content": "coming soon",
+        "category": "Text Editor",
+        "level": "Beginner",
+        "__v": 0
+    },
+    {
+        "title": "Text Intro",
+        "user_id": "5b43b25044e210cc34e8ab0e",
+        "content": "coming soon",
+        "category": "Text Editor",
+        "level": "Beginner",
+        "upvotes": 2,
+        "downvotes": 6,
+        "__v": 0
+    },
+    {
+        "title": "Text Editor Intro",
+        "user_id": "5b43b25044e210cc34e8ab0e",
+        "content": "coming soon",
+        "category": "Text Editor",
+        "level": "Beginner",
+        "__v": 0
+    },
+    {
+        "title": "Text Editor Customization",
+        "user_id": "5b43b25044e210cc34e8ab0e",
+        "content": "coming soon",
+        "category": "Text Editor",
+        "level": "Intermediate",
+        "__v": 0
+    },
+    {
+        "title": "Text Editor Linting",
+        "user_id": "5b43b25044e210cc34e8ab0e",
+        "content": "coming soon",
+        "category": "Text Editor",
+        "level": "Advanced",
+        "__v": 0
+    },
+    {
+        "title": "Text Editor opening files from CLI",
+        "user_id": "5b43b25044e210cc34e8ab0e",
+        "content": "coming soon",
+        "category": "Text Editor",
+        "level": "Intermediate",
+        "__v": 0
+    }
+]
+
+module.exports = {posts}
